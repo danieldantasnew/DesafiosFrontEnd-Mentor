c5c31d16712c0f9fdc3c32125ad84bce401e8b8c
Daniel Dantas <xxxxxdanielzimmxxxx.danieldant@gmail.com>
2023-04-16 02:53:49 -0300

    correções de bugs

--- DesafioNovato/RealizaProjetos/ProjetoAvaliação/script.js
+++ DesafioNovato/RealizaProjetos/ProjetoAvaliação/script.js
@@ -1,38 +1,22 @@
+var recebeNota;
+
 function mudaCor(){
     const hover = document.querySelectorAll('.bg-numbers');
-
     function mudaBG(){
-        hover.forEach(numero => {
-            numero.style.backgroundColor = 'var(--azul-escuro)';
-            numero.style.color = 'var(--cinza-claro)';
-        });
-        
-        this.style.backgroundColor = 'var(--laranja)';
-        this.style.color = 'var(--branco)';
+        hover.forEach((numero) =>{
+            numero.classList.remove('ativo');
+        })
+        this.classList.add('ativo');
+
+        let converteNota = parseInt(this.textContent);
+        recebeNota = converteNota;
     }
 
-    hover.forEach( (numero) => {
+    hover.forEach((numero) => {
         numero.addEventListener('click', mudaBG);
     });
+
+    
 }
 
 mudaCor();
-
-function pegaNota(){
-    const texto = document.querySelector('#bg-para');
-    const hover = document.querySelectorAll('.bg-numbers');
-    let nota = 0;
-
-    // function darNota(index){
-    //     nota = index + 1;
-    //     texto.innerText = `You selected ${nota} out of 5`;
-    // }
-
-    // hover.forEach((numero, index) => {
-    //     numero.addEventListener('click', ()=>{
-    //         darNota(index);
-    //     })
-    // })
-}
-
-pegaNota();
