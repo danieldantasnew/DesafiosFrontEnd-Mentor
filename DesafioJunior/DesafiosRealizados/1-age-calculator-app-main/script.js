bcd127d23111f796370c04389cce51aa3f712c24
Daniel Dantas <xxxxxdanielzimmxxxx.danieldant@gmail.com>
2023-04-21 04:35:14 -0300

    novas alterações

--- DesafioJunior/DesafiosRealizados/1-age-calculator-app-main/script.js
+++ DesafioJunior/DesafiosRealizados/1-age-calculator-app-main/script.js
@@ -3,18 +3,39 @@ function botaoClicado(){
     const inputDia = document.querySelector('#dia');
     const inputMes = document.querySelector('#mes');
     const inputAno = document.querySelector('#ano');
-    const data = new Date();
+    const dataAtual = new Date();
     const textoIdade = document.querySelectorAll('.idade');
-    console.log(data.getFullYear())
     
-    function pegaEntrada(){
+    function pegaEntrada(evento){
+        evento.preventDefault(); //evita que a página seja recarregada após o envio do formulário
         let valorDia = inputDia.value;
         let valorMes = inputMes.value;
-        let valorAno = data.getFullYear() - (inputAno.value);
+        let valorAno = inputAno.value;
 
-        textoIdade[0].innerHTML = `${valorAno}`
-        textoIdade[1].innerHTML = `${valorMes}` 
-        textoIdade[2].innerHTML = `${valorDia}` 
+        function calculaIdade(ano, mes, dia){
+            let dataNascimento = new Date(ano, mes-1, dia);
+            let diferenca = dataAtual - dataNascimento;
+
+            const anos = Math.floor(diferenca / 1000 / 60 / 60 / 24 / 365);
+            textoIdade[0].innerHTML = `${anos}`;
+
+            const meses = Math.floor((diferenca / 1000 / 60 / 60 / 24) % 365 / 30);
+            textoIdade[1].innerHTML = `${meses}`;
+
+            const dias = Math.floor((diferenca / 1000 / 60 / 60 / 24) % 365 % 30);
+            textoIdade[2].innerHTML = `${dias}`;
+
+            /*mes e dias estão errados
+            - Fazer verificação se a data existe, por exemplo 31/04/2023 não existe
+
+            - Não permitir que seja enviado dados vazios
+
+            - Arrumar mobile
+
+            */
+        }
+
+        calculaIdade(valorAno, valorMes, valorDia);
     };
 
     botaoSubmit.addEventListener('click', pegaEntrada);
